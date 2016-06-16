002cc90aa73467deea496d049ca577e544161f8f
gerry <weirich@elexis.ch>
2016-06-16 09:46:09 +0200

    corr

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ mkdir -p ${P2}/${BUILD_NUMBER}
 cp -R ch.elexis.base.p2site/target/repository/* ${P2}/${BUILD_NUMBER}
 
 rm ${P2}/latest
-ln -s ${BUILD_NUMBER} ${WEBSPACE}/p2/elexis-base/latest
+ln -s ${P2}/${BUILD_NUMBER} ${P2}/latest
